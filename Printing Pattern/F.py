c6e10bcecd600c9c9a322141f199ccd0a707bf9f
Md. Abdullah-Al Mamun <mamun.cou13@gmail.com>
2020-09-30 00:47:23 +0300

    Add files via upload

---
+++ Printing Pattern/F.py
@@ -0,0 +1,7 @@
+for row in range(0,7):
+    for col in range(0,4):
+        if ((row==0  or col==0 )or(row==3)):
+            print('#', end='')
+        else:
+            print( end=" ")
+    print()
