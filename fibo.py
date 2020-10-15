fdfafc33aa1ec3061a1599492b439dcdc5795256
Md. Abdullah-Al Mamun <mamun.cou13@gmail.com>
2020-10-15 10:51:35 +0300

    Add files via upload

---
+++ fibo.py
@@ -0,0 +1,15 @@
+def main():
+    enter = int(input("How many Fibonacci numbers do you want? "))
+    a, b = 0, 1
+    c = 0
+    while True:
+        a, b = b, a + b
+        c += 1
+        if c == (enter + 1):
+            break
+        else:
+            print("{}. {}".format(c, a))
+
+
+if __name__ == "__main__":
+    main()
