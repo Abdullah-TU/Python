7fb78eda0255134b4604d76ddcbcf7fe8bd67512
Md. Abdullah-Al Mamun <mamun.cou13@gmail.com>
2020-10-19 05:12:56 +0300

    Add files via upload

---
+++ Rock_Paper_Scissors.py
@@ -0,0 +1,29 @@
+def main():
+    player1 = input("Player 1, enter your choice (R/P/S): ")
+    player2 = input("Player 2, enter your choice (R/P/S): ")
+
+
+    if player1 == "P" and player2 == "S":
+        print("Player 2 won!")
+
+    elif player1 == "S" and player2 == "P":
+        print("Player 1 won!")
+
+    elif player1 == "P" and player2 == "R":
+        print("Player 1 won!")
+
+    elif player1 == "R" and player2 == "P":
+        print("Player 2 won!")
+
+    elif player1 == "R" and player2 == "S":
+        print("Player 1 won!")
+
+    elif player1 == "S" and player2 == "R":
+        print("Player 2 won!")
+
+    else:
+        print("It's a tie!")
+
+
+if __name__ == "__main__":
+    main()
