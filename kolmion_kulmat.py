62acb4ce712ae8b1f86acbf4cfce8cee7bc23a9a
Md. Abdullah-Al Mamun <mamun.cou13@gmail.com>
2022-07-23 15:13:54 +0300

    Add files via upload

---
+++ kolmion_kulmat.py
@@ -0,0 +1,24 @@
+"""
+COMP.CS.100 Programming 1
+Triangle's angle
+"""
+
+def calculate_angle(x, y=False):
+    
+    """
+    passing optional named arguments by position to find out the third angle of triangle.
+    
+    Note: the sum of triangle's corner angles is 180°.
+    """
+    
+    if y is not False :
+        n=int(y)
+        f=180
+        m=f-(int(x)+n)
+        return m
+    if y is False:
+        x=int(x)
+        z=90-x          
+        return z
+        
+calculate_angle(30,78)
