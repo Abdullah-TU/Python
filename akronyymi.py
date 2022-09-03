7bb840e91c70802f976ba9a726ad973acafaccdf
Md. Abdullah-Al Mamun <mamun.cou13@gmail.com>
2022-09-03 10:57:06 +0300

    Update akronyymi.py

--- akronyymi.py
+++ akronyymi.py
@@ -2,8 +2,8 @@
 Reverse the Names to Correct Order
 
 : Implementing the function create_an_acronym, which requests a name as a parameter and returns its acronym. 
-
-and some other conditions......
+  and some other conditions...
+  
 """
 
 def create_an_acronym(lists):
