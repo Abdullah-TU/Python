341b4712965d7907f1809bc7b6ab579232803f54
Md. Abdullah-Al Mamun <mamun.cou13@gmail.com>
2022-09-25 05:26:24 +0300

    Update str_dokumentaatio.py

--- str_dokumentaatio.py
+++ str_dokumentaatio.py
@@ -7,12 +7,10 @@ each word starting in upper case but the rest of the world in lower case.
 """
 def capitalize_initial_letters(capital=""):
     """
-    : firstly making the provided string into lower case.   
-       
+    : firstly making the provided string into lower case.          
     : then using title() method to capitalize first letter of each word.
     
-    """    
-    
+    """        
     x=capital.lower()
     return x.title()              
             
