1dc5124938e403447548f9337b08a3faab90d765
Md. Abdullah-Al Mamun <mamun.cou13@gmail.com>
2022-09-25 05:25:29 +0300

    Update str_dokumentaatio.py

--- str_dokumentaatio.py
+++ str_dokumentaatio.py
@@ -1,5 +1,5 @@
 """
-Capitalization
+: Capitalization
 
 : Creating the function capitalize_initial_letters, which uses a string as a parameter and returns it as written, with 
 each word starting in upper case but the rest of the world in lower case.
