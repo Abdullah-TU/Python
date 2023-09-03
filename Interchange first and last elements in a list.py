c99258071fcd5aa62cdf26990d5803c976bbd89e
Md. Abdullah-Al Mamun <mamun.cou13@gmail.com>
2023-09-03 19:14:53 +0300

    Add files via upload

---
+++ Interchange first and last elements in a list.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Interchange first and last elements in a list
+# -  [1, 2, 3]
+# - [3, 2, 1]
+
+# In[ ]:
+
+
+def interchange(list):
+    
+
+
+# In[7]:
+
+
+x = [1, 2, 3]
+
+x.reverse()
+
+x
+
+
+# In[17]:
+
+
+def interchange(newList):
+   print("Original List:", newList)
+   newList.reverse()
+   print("Reversed List:", newList)
+
+# Driver code
+m = [12, 35, 9, 56, 24]
+
+interchange(m)
+
+
+# In[ ]:
+
+
+
+
